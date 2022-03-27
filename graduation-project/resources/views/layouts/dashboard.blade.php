7c11034f9384bdabbbd91381972365517795efbb
Osama Hamad <59566963+osamahamad@users.noreply.github.com>
2022-03-27 18:46:39 +0300

    userway 3rd party

--- graduation-project/resources/views/layouts/dashboard.blade.php
+++ graduation-project/resources/views/layouts/dashboard.blade.php
@@ -42,6 +42,9 @@
     </head>
 
     <body>
+    <!-- userway 3rd party -->
+        <script>(function(d){var s = d.createElement("script");s.setAttribute("data-account", "VzizBb3ep2");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)</script><noscript>Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a></noscript>    
+        <!-- userway 3rd party -->
         <div id="app">
 
             <!-- Section: Header -->
